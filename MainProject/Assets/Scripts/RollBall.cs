5747da72f2375d3d4443ce170f91424dc022ad86
Tristan Jardon <tristan.jardon@gmail.com>
2018-01-12 16:33:43 +0100

    Roll a supo

---
+++ MainProject/Assets/Scripts/RollBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RollBall : MonoBehaviour {
+
+    [SerializeField] [Range(0, 360)] private float x_axe;
+    [SerializeField] [Range(0, 360)] private float y_axe;
+    [SerializeField] [Range(0, 360)] private float z_axe;
+
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        transform.Rotate(new Vector3(x_axe, y_axe, z_axe));
+    }
+}
